82e85075d31fcd3640ecdb879f1aac3477712a97
unknown <lepotoman@msn.com>
2021-12-21 23:17:35 +0100

    Print teapot.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ extern crate glium;
 use glium::uniform;
 use glium::Surface;
 
+#[path = "teapot.rs"]
+mod teapot;
+
 fn main() {
     use glium::glutin;
     // 1. The **winit::EventsLoop** for handling events.
@@ -20,56 +23,41 @@ fn main() {
     //    window with the events_loop.
     let display = glium::Display::new(wb, cb, &events_loop).unwrap();
 
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 2],
-    }
-    glium::implement_vertex!(Vertex, position);
-
-    let vertex1 = Vertex {
-        position: [-0.5, -0.5],
-    };
-    let vertex2 = Vertex {
-        position: [0.0, 0.5],
-    };
-    let vertex3 = Vertex {
-        position: [0.5, -0.25],
-    };
-    let shape = vec![vertex1, vertex2, vertex3];
-
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
+    let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    let indices = glium::IndexBuffer::new(
+        &display,
+        glium::index::PrimitiveType::TrianglesList,
+        &teapot::INDICES,
+    )
+    .unwrap();
 
     let vertex_shader_src = r#"
     #version 140
 
-    in vec2 position;
-    out vec2 my_attr;      // our new attribute
-
+    in vec3 position;
+    in vec3 normal;
+    
     uniform mat4 matrix;
-
+    
     void main() {
-        my_attr = position;     // we need to set the value of each `out` variable.
-        gl_Position = matrix * vec4(position, 0.0, 1.0);
+        gl_Position = matrix * vec4(position, 1.0);
     }
 "#;
 
     let fragment_shader_src = r#"
-#version 140
+    #version 140
 
-in vec2 my_attr;
-out vec4 color;
-
-void main() {
-    color = vec4(my_attr, 0.0, 1.0);   // we build a vec4 from a vec2 and two floats
-}
+    out vec4 color;
+    
+    void main() {
+        color = vec4(1.0, 0.0, 0.0, 1.0);
+    }
 "#;
 
     let program =
         glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
             .unwrap();
-
-    let mut t: f32 = -0.5;
 
     events_loop.run(move |ev, _, control_flow| {
         let next_frame_time =
@@ -86,29 +74,22 @@ void main() {
             _ => (),
         }
 
-        // we update `t`
-        t += 0.0002;
-        if t > 0.5 {
-            t = -0.5;
-        }
-
-        let uniforms = uniform! {
-            matrix: [
-                [ t.cos(), t.sin(), 0.0, 0.0],
-                [-t.sin(), t.cos(), 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 1.0f32],
-            ]
-        };
-
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        let matrix = [
+            [0.01, 0.0, 0.0, 0.0],
+            [0.0, 0.01, 0.0, 0.0],
+            [0.0, 0.0, 0.01, 0.0],
+            [0.0, 0.0, 0.0, 1.0f32],
+        ];
+
         target
             .draw(
-                &vertex_buffer,
+                (&positions, &normals),
                 &indices,
                 &program,
-                &uniforms,
+                &uniform! { matrix: matrix },
                 &Default::default(),
             )
             .unwrap();
